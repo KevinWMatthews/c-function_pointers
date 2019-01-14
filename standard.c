fe40d7f19ce887113494896c322d8696474dc055
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-01-13 21:11:00 -0800

    Standard function pointer example

--- standard.c
+++ standard.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
+
+void function(void)
+{
+    printf("Executing %s\n", __func__);
+}
+
 int main(void)
 {
+    void (*function_pointer)(void) = function;
+    function_pointer();
+
     return 0;
 }
